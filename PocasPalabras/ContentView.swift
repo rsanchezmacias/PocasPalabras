10efdecb7928aba53ba7b4c50666683111158928
Ricardo Sanchez <r.sanchez.macias.dev@gmail.com>
2025-06-13 23:32:17 -0700

    Initial Commit

---
+++ PocasPalabras/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocasPalabras
+//
+//  Created by Ricardo Sanchez-Macias on 6/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocasPalabras/PocasPalabrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocasPalabrasApp.swift
+//  PocasPalabras
+//
+//  Created by Ricardo Sanchez-Macias on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PocasPalabrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
